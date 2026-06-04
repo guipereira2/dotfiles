a57c11da9e439c37e6def5f2855d811fd57930a6
guipereira2 <guilherme.campos24@unifesp.br>
2026-06-04 16:58:54 -0300

    wip: neovim

--- nvim/lua/plugins/roslyn.lua
+++ nvim/lua/plugins/roslyn.lua
@@ -1,10 +1,15 @@
--- Roslyn-based LSP for C# (Microsoft.CodeAnalysis.LanguageServer)
--- The server binary is installed via Mason (see mason.lua: "roslyn")
-
 ---@type LazySpec
 return {
   "seblyng/roslyn.nvim",
   ft = "cs",
+  init = function()
+    local d = vim.lsp.diagnostic
+    if d and not d._refresh then
+      d._refresh = function(opts)
+        if d._workspace_diagnostics then pcall(d._workspace_diagnostics, opts or {}) end
+      end
+    end
+  end,
   ---@module 'roslyn.config'
   ---@type RoslynNvimConfig
   opts = {},
