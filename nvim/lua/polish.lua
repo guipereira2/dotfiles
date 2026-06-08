00e187ab219c0fd96b7875478a7c18442821e4ca
guipereira2 <guilherme.campos0024@gmail.com>
2026-06-08 12:12:50 -0300

    wip: nvim

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,4 +1,4 @@
 local reload_timer = vim.uv.new_timer()
-reload_timer:start(500, 500, vim.schedule_wrap(function()
+reload_timer:start(200, 200, vim.schedule_wrap(function()
   if vim.fn.getcmdwintype() == "" then vim.cmd "silent! checktime" end
 end))
