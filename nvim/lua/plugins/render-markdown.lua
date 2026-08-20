1c1a414922f7ba8a982b0ce36f22b438d6945e09
guipereira2 <guilherme.campos0024@gmail.com>
2026-08-20 17:26:51 -0300

    zed as my main IDE

--- nvim/lua/plugins/render-markdown.lua
+++
@@ -1,9 +0,0 @@
----@type LazySpec
-return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  ft = { "markdown" },
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
-}
--- nvim/lua/polish.lua
+++
@@ -1,4 +0,0 @@
-local reload_timer = vim.uv.new_timer()
-reload_timer:start(200, 200, vim.schedule_wrap(function()
-  if vim.fn.getcmdwintype() == "" then vim.cmd "silent! checktime" end
-end))
